b96d0b9bde966da8959a808f8b9bea1a59896c46
Hushnudbek <tobymarshal2802@gmail.com>
2022-08-30 18:09:25 +0500

    Synchronization Implementation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/sync')
+  async sync(): Promise<object> {
+    return this.appService.sync();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'ХУШНУДБЕК зовут!';
+  }
+
+  async sync(): Promise<object> {
+    return { message: 'Synchronization successfully EXECUTED!' };
   }
 }
